fbd981996aa3ad245db4fcfa86ac4d15e5e4e9ae
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2024-05-18 21:04:39 +0530

    feat: add data population logic for movies & tv shows

--- src/core/scripts/populate-core-data.script.ts
+++ src/core/scripts/populate-core-data.script.ts
@@ -4,6 +4,9 @@ import { LoggingService } from 'src/log/service/log.service';
 import { ErrorHandlerService } from 'src/error/service/error-handler.service';
 import { ErrorCodes } from 'src/error/constants/error-codes';
 import { CustomErrorException } from 'src/error/exceptions/custom-error.exception';
+import { dummyMovies, dummyTvShows, dummyUser } from '../data/dummy.data';
+import { MovieRepository } from '../repositories/movie.repository';
+import { TvShowRepository } from '../repositories/tv-shows.repository';
 
 @Injectable()
 export class PopulateCoreDataScript {
@@ -11,6 +14,8 @@ export class PopulateCoreDataScript {
 
   constructor(
     private userRepository: UserRepository,
+    private movieRepository: MovieRepository,
+    private tvShowRepository: TvShowRepository,
     private loggingService: LoggingService,
     private errorHandlerService: ErrorHandlerService,
   ) {
@@ -21,25 +26,64 @@ export class PopulateCoreDataScript {
    * Populate the db with dummy user
    *
    */
-  public async populateUser(secret: string): Promise<boolean> {
+  public async populateUser(secret: string): Promise<String> {
     this.validateSecret(secret);
 
-    const favoriteGenres: Genre[] = ['Action', 'Comedy', 'SciFi'];
-    const dislikedGenres: Genre[] = ['Horror'];
-
-    const dummyUser: UserDto = {
-      username: 'Rishabh',
-      preferences: { dislikedGenres, favoriteGenres },
-      watchHistory: [],
-    };
+    const user: UserDto = dummyUser;
     try {
-      const createdUser = await this.userRepository.save(dummyUser);
+      const createdUser = await this.userRepository.save(user);
       this.loggingService.logInfo('Created dummy user', createdUser);
-      return true;
+      return 'Successfully populated user!';
     } catch (err) {
       this.errorHandlerService.handleError(ErrorCodes.EC_0000, err);
     }
-    return false;
+    return 'Failed to populate user!';
+  }
+
+  /**
+   * Populate the db with dummy user
+   *
+   */
+  public async populateMovies(secret: string): Promise<String> {
+    this.validateSecret(secret);
+    const movies: MovieDto[] = dummyMovies;
+
+    try {
+      const createdMovieDocuments = await this.movieRepository.saveMovies(
+        movies,
+      );
+      this.loggingService.logInfo(
+        'Populated dummy movies count',
+        createdMovieDocuments.length,
+      );
+      return 'Successfully populated movies!';
+    } catch (err) {
+      this.errorHandlerService.handleError(ErrorCodes.EC_0000, err);
+    }
+    return 'Failed to populate movies!';
+  }
+
+  /**
+   * Populate the db with dummy user
+   *
+   */
+  public async populateTvShows(secret: string): Promise<String> {
+    this.validateSecret(secret);
+    const tvShows: TVShowDto[] = dummyTvShows;
+
+    try {
+      const createdTvShowDocuments = await this.tvShowRepository.saveTvShows(
+        tvShows,
+      );
+      this.loggingService.logInfo(
+        'Populated dummy tv shows count',
+        createdTvShowDocuments.length,
+      );
+      return 'Successfully populated tv shows!';
+    } catch (err) {
+      this.errorHandlerService.handleError(ErrorCodes.EC_0000, err);
+    }
+    return 'Failed to populate tv shows!';
   }
 
   private validateSecret(secret: string): boolean {
