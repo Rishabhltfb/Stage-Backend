1842f1cddcdc1eec011a77a78c56675e0277c235
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2024-05-18 15:01:04 +0530

    chore: add user entity

---
+++ src/core/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { BaseModel } from 'src/interface/repository/base.model';
+
+@Schema({ minimize: false, timestamps: true, id: true })
+export class User extends BaseModel {
+  @Prop({ type: String, unique: true, required: true })
+  username: string;
+
+  @Prop({ required: true, type: Object })
+  preferences: Preferences;
+
+  @Prop({ required: true, type: Array<Object> })
+  watchHistory: Array<WatchHistory>;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+UserSchema.index({ username: 1 });
