9359e24ea97baf98333f56311d206849e1ee090c
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2024-05-18 17:22:57 +0530

    fix: env fallback issue

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { RequestLoggingInterceptor } from './util/interceptors/request-logging.interceptor';
 
-const DEFAULT_PORT = 3003;
-const DEFAULT_ENV = 'dev';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new RequestLoggingInterceptor());
 
-  await app.listen(3003).then(() => {
-    console.log(
-      `Listening at port: ${process.env.PORT ?? DEFAULT_PORT} in ${
-        process.env.STAGE ?? DEFAULT_ENV
-      } env`,
-    );
+  const PORT = process.env.PORT ?? 3003;
+  const ENV = process.env.STAGE ?? 'dev';
+
+  await app.listen(PORT).then(() => {
+    console.log(`Listening at port: ${PORT} in ${ENV} env`);
   });
 }
 bootstrap();
