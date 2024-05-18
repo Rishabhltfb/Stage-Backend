7e7935d39b7c172d1ca96d72460416829280170e
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2024-05-18 11:46:40 +0530

    chore: add basic env configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const DEFAULT_PORT = 3003;
+const DEFAULT_ENV = 'dev';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(3003).then(() => {
+    console.log(
+      `Listening at port: ${process.env.PORT ?? DEFAULT_PORT} in ${
+        process.env.STAGE ?? DEFAULT_ENV
+      } env`,
+    );
+  });
 }
 bootstrap();
