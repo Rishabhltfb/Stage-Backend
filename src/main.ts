534ac14d4cc7bc9dbc8bf6beb39856ae89fd21f8
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2024-05-18 12:44:47 +0530

    feat: activate global validation pipe & req interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { RequestLoggingInterceptor } from './util/interceptors/request-logging.interceptor';
 
 const DEFAULT_PORT = 3003;
 const DEFAULT_ENV = 'dev';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new RequestLoggingInterceptor());
 
   await app.listen(3003).then(() => {
     console.log(
