2cf3fe06088c9dd606b5fc3df330356406b606d5
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2024-05-18 15:04:17 +0530

    feat: connect cloud mongodb with app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [],
+      envFilePath: `.env.${process.env.STAGE}`,
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.n5yokp0.mongodb.net/${process.env.DB_Name}`,
+    ),
+    CoreModule,
+  ],
   controllers: [],
   providers: [],
 })
