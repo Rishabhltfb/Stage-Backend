f29894da83bbcc9f91baff5808722f9873beda7f
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2024-05-19 00:32:07 +0530

    ref: entity & dto naming

--- src/my-list/dtos/request/add-item-req.dto.ts
+++ src/my-list/dtos/request/add-item-req.dto.ts
@@ -1,7 +1,7 @@
 import { IsEnum, IsString } from 'class-validator';
 import { ContentType } from 'src/my-list/enums/content-type.enum';
 
-export class AddItemRequestBody {
+export class AddListItemRequestBody {
   @IsString()
   contentId: string;
 
